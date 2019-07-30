c33fea058fa46fa05f3dc9dc64474f36e3ef68c1
Dmytro Antoshkiv <antoshkiv1@gmail.com>
2019-07-30 09:35:06 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, bindActionCreators } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
+import App from './components/app';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
